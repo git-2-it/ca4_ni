49a19c22e96b0b0dc84e0872b8399c9f1f997eb1
git-2-it <59895978+git-2-it@users.noreply.github.com>
2020-05-23 19:54:01 +0100

    0.6

--- ca4_ Prescription_Trends.R
+++ ca4_ Prescription_Trends.R
@@ -19,6 +19,8 @@ colSums(is.na(data_in))
 # make sure to check ordering
 data_in <- data_in %>% arrange(Year, Month)
 
+head(data_in)
+
 # Monthly items prescribed from April 2013 until latest figures available, Feb 2020
 # frequnecy = no of obvs per unit in time
 
@@ -51,56 +53,6 @@ opar <- par(no.readonly = FALSE)
 # install.packages("forecast")
 library(forecast)
 
-# # run the auto armina model as a reference point
-# auto_arima_model <- auto.arima(ts_monthly_ratio)
-# auto_arima_model
-# 
-# forecast_model <- forecast(auto_arima_model, 
-#                            level = c(95), # 36 momnths (though could be 3 * 16), 
-#                            h = 8) # h is confidence interval
-# forecast_model
-# plot(forecast(forecast_model, 4))
-# 
-# 
-
-
-opar <- par(no.readonly = TRUE)
-
-par( mfrow = c(2,2))
-
-ylimit <- c(min(ts_monthly), max(ts_monthly))
-
-# ma() allows smoothing of the data (moving average)
-
-plot(ma(ts_monthly, 3),      main = "ts_monthly MA, 3",     ylim = ylimit )
-plot(ma(ts_monthly, 7),      main = "ts_monthly MA, 7",     ylim = ylimit )
-plot(ma(ts_monthly, 15),     main = "ts_monthly MA, 15",    ylim = ylimit )
-
-par(opar)
-
-# All moving average plots show upwards trend
-
-# stationarity check
-# Test the stationarity of the object
-# augmented Dicky-Fuller test - ADF test
-# H0 - the time series is non-stationary
-# H1 - the time series is stationary
-adf.test(ts_monthly)
-# adf.test( ts_monthly, alternative = "stationary", k= 12)
-kpss.test(ts_monthly)
-# Also indicates stationary
-# Trend stationary?
-
-
-nsdiffs(ts_monthly)
-# No diffs necessary
-
-pp.test(ts_monthly)
-
-
-# low p-value of less than 0.01 indicates the data is a stationary dataset
-# that is, mean, variance etc don't change over time.
-
 # is the data multiplicative or additive ?
 # additive, when the data is trend plus the seasonality 
 # multiplicative, trend still there and seasonailty, but the seasonality range (peak to trough) is changing
@@ -115,12 +67,26 @@ plot(ts_monthly,
 abline( reg = lm(ts_monthly ~ time(ts_monthly) ) )
 
 # Obvious upward trend as the years progress
-# Indicates an increasing number of items are prescribed each year even if 
-# patient/popoulation remained stable
+# This would indicates an increasing number of items are prescribed each year
+# even if patient numbers/popoulation remained stable
+
+opar <- par(no.readonly = TRUE)
+
+par( mfrow = c(2,2))
+
+ylimit <- c(min(ts_monthly), max(ts_monthly))
+
+# ma() allows smoothing of the data (moving average)
+
+plot(ma(ts_monthly, 3),      main = "ts_monthly MA, 3",     ylim = ylimit )
+plot(ma(ts_monthly, 7),      main = "ts_monthly MA, 7",     ylim = ylimit )
+plot(ma(ts_monthly, 15),     main = "ts_monthly MA, 15",    ylim = ylimit )
+plot(aggregate(ts_monthly, FUN = mean))
+
+par(opar)
 
 # Get the YoY trend
 # whats adding to the data year on year
-plot(aggregate(ts_monthly, FUN = mean))
 
 
 # seasonality - do things change in the monthly periods
@@ -129,13 +95,6 @@ boxplot(ts_monthly ~ cycle(ts_monthly),
         xlab = "Month",
         ylab = "Item Ratio",
         main = "Monthly Item/Patient Ratio")
-
-boxplot(clean_month ~ cycle(clean_month),
-        xlab = "Month",
-        ylab = "Item Ratio",
-        main = "Monthly Item/Patient Ratio")
-
-clean_month
 
 # Note the outliers - what are they?
 
@@ -147,233 +106,241 @@ chk
 
 # Looks like there is a seasonal component, ie the data changes from month to month
 
-seasonal_decomp <- stl(ts_monthly, 
-                       s.window = "period")
-
-plot(seasonal_decomp)
-
-# Can see the seasonal nature of the data
-# A series of peaks at certain times of the year, notablly around 
-# year end, followed by a sharp trough, except around teh middle of
-# the year, when figures almost (ut not quite) plateau, or at least
-# decline more slowly. Digging into the natures of teh items might be
-# illustrative to show why there are peaks, eg could peaks be cold/flu 
-# related in winter, with sharp drop off if practices are closed over the
-# festive season, while in summer could there a more sustained demand for 
-# hayfever treatmetns
-
 # Multiplicative or additive?
-# Looks additive, that is there is a year trend, and a season trend, but the
-# season trend is not changing over time
+# Looks additive, that is there is a year trend, and a season trend, 
+# and the season trend is not changing over time
 
-# compare plot and log log plot?
+# Check, by comparing compare plot and log plot?
+
 
 log_ts <- log(ts_monthly)
-plot(log_ts)
 
-# par(mfrow = c(1,2))
 plot(ts_monthly)
 par(new = TRUE)
-plot(log_ts, type = "l", axes = FALSE, bty = "n", xlab = "", ylab = "", col = "red")
+plot(log_ts, type = "l", axes = FALSE, bty = "n", xlab = "", ylab = "", col = "red",
+     main = "Data plot with log of data overlay")
 
 par(opar)
 
-# Still looks additive - the base plot and the log plot look the same.
+# nsdiffs(ts_monthly) # Value of zero matches the observation
+# Data still looks additive - the base plot and the log plot look the same.
 
 
-clean_month <- tsclean(ts_monthly) 
+######################
+# Decompose the data
+######################
+
+ts_decompose <- stl(ts_monthly, s.window="periodic")
+
+deseason_ct <- seasadj(ts_decompose)
+
+plot (ts_decompose)
+
+# stationarity check
+# Test the stationarity of the object
+# augmented Dicky-Fuller test - ADF test
+# H0 - the time series is non-stationary
+# H1 - the time series is stationary
+adf.test( ts_monthly, alternative = "stationary")
+
+# suggested_k <- trunc((length(ts_monthly)-1)^(1/3))
+# suggested_k
+
+# Indicates its stationary, can see from trend its not
+# Reset the test to be monthly, ie lag order of 12 not the calculated 4
+adf.test( ts_monthly, alternative = "stationary", k= 12)
+
+# Not stationary at this lag order
+
+# Check the auto-corellation
 
 #lags - how correlated a time series is with its previous values
 acf(ts_monthly,
     main = "Item/Patient ratio\nAuto-corellation")
-acf(clean_month,
-    main = "Item/Patient ratio\nAuto-corellation")
 
 # Acf(my_pax) --- don't use the upper case A model
-# Indicates non-stationary? Hhmmmm
-# Multiple lag points?
+# Indicates non-stationary, no immediate tail off to zero
+# Also 
 
 # Partial auto-cor
 Pacf(ts_monthly,
      main = "Item/Patient ratio\nPartial auto-corellation"
 )
 
-# Each cross over the blue dotted line indicates a potential al at that point
+# Need to de-trend and de-seasonalise the data
+
+# De-trend the data
+# Its additive already, no need for log diffs
+
+nsdiffs(ts_monthly)
+# 0
+
+nsdiffs(ts_monthly)
+
+diff_ts_monthly <- diff(ts_monthly, lag = 12 , differences = 1)
+
+adf.test(diff_ts_monthly, alternative = "stationary")
+# low p-value = 0.04819 - Stationary
 
 
-plot(ts_monthly)
-trained_model<-lm(ts_monthly~ c(1:length(ts_monthly)))
-# de-trended series.
-plot(resid(trained_model), type = "l")
+acf(ts_monthly)
+Pacf(ts_monthly)
 
 
-ts_decompose<-stl(ts_monthly, "periodic")
-# de-seasonalisethe time series
-ts_seasonal_adjust<-seasadj(ts_decompose)
-# original series
-plot(ts_monthly, type = "l")
-# seasonal adjusted
-plot(ts_seasonal_adjust, type = "l")
-
-# Plot showing monthly ratio, by year
-seasonplot(ts_seasonal_adjust, 
-           12, 
-           col = rainbow(12), 
-           year.labels= TRUE, 
-           main = "Seasonally adjusted: Item/Patient ratio")
-# Shows the YoY upwards trend
-
-
-# get rid of seasonality
-ts_monthly_log <- log(ts_monthly)
-plot(ts_monthly_log)
-
+# Which ARIMA model to use?
+# Collect the decision points
+# Corellelation at lag one for dataset = negative - indicates the AR model
+# ACF at lag n - drops of at 0 (ie drops below threshold after 0) - p
+# PACF at lag n - drops of at 2 (ie drops below threshold after 2) - q
+# d = number of diffs, 1
+# (0, 1, 2)
 par(mfrow = c(1,2))
-plot(ts_monthly)
-plot(ts_monthly_log)
 
-par(opar)
+acf(ts_monthly) 
+# significant at 0 - P
+Pacf(ts_monthly)
+# significant at 1 - Q
+ndiffs(ts_monthly)
+# 1 - D
 
+# Seasonal - (P, D, Q)[frequency]
+# frequency = 12
+acf(diff_ts_monthly) 
+# significant at 0 - P
+Pacf(diff_ts_monthly)
+# significant at 1
+ndiffs(diff_ts_monthly)
+# 0 - D
+# (0, 0, 1)
 
-adf.test( ts_monthly_log, alternative = "stationary")
+# total model = (0, 1, 2)(0, 0, 1)[12]
 
-acf(ts_monthly_log)
-
-# peaks and troughs in Acf means there are seasonality, want it to be more random
-
-ndiffs(ts_monthly_log)
-# needs to moved one to teh left
-
-ts_monthly_diff <- diff(ts_monthly_log, lag = 1)
-
-# check if data needs further differencing
-ndiffs(ts_monthly_diff)
-
-# now is zero, so now more needed
-
-acf(ts_monthly_diff)
-
-# plot(diff_pax)
-
-# summary
-# log_pax - unequal variances in the pax data
-# diff_pax - lagged air passengers
-
-
-# ARIMA models
-# Use the acf plot to figure which to use 
-# +ve in acf in lag 1 then use AR model
-# -ve in acf in lag 1 then use MA model
-
-Pacf(ts_monthly_diff)
-
-# Use the Pacf plot to figure which to use 
-# +ve in acf in lag 1 then use AR model
-# -ve in acf in lag 1 then use MA model
-
-# SARIMA(p, d, q)
-# p - no if autoregressive
-# d = degree of differencing (d = 1)
-# q = the no of moving average terms
-
-# SARIMA(p, d, q)(P, D, Q)m
-# p - no if autoregressive
-# d = degree of differencing (d = 1)
-# q = the no of moving average terms
-# m = no of periods in the season
-
-
-# degree of differencing, d, is 1, the no of lags
-
-# SARIMA(0, 1, 1)(0, 1, 1)12
-
-# using log in this case because of smoothing effect
-fit <-arima(ts_monthly_log, 
-            c(0, 1, 1),
-            seasonal = list(order = c(0, 1, 1),
+arima_items <-arima(ts_monthly, 
+            c(0, 1, 2),
+            seasonal = list(order = c(1, 0, 0),
                             period = 12)
 )
-fit
+arima_items
 
 # generate some forecasts
 # 2 ways
 # predict
-prediction <- predict(fit, n.ahead = 3 * 12 ) # 3 years at 12 months
+prediction <- predict(arima_items, n.ahead = 3 * 12 ) # 3 years at 12 months
 prediction
 
-forecast_model <- forecast(fit, 
+forecast_model <- forecast(arima_items, 
                            level = c(95), # 36 momnths (though could be 3 * 16), 
-                           h = 36) # h is confidence interval
+                           h = 18) # h is confidence interval
 forecast_model
 
+plot(forecast_model)
 
-plot(forecast(forecast_model, 3))
 
-auto_arima_model <- auto.arima(ts_monthly)
+#------------------------------------------
+# Model Evaluation
+#------------------------------------------
+
+# Use the auto generate function
+auto_model <- auto.arima(ts_monthly)
+auto_model
+
+# Different model proposed - ARIMA(5,1,0)(1,0,0)[12]
+
+# Which is better?
+
+qqnorm(arima_items$residuals)
+qqline(arima_items$residuals)
+
+qqnorm(auto_model$residuals)
+qqline(auto_model$residuals)
+
+
+# Look similar ...
+
+Box.test(arima_items$residuals, type = "Ljung-Box")
+Box.test(auto_model$residuals, type = "Ljung-Box")
+
+
+# Manually selected model has a better p-value
+# manual        - 0.6194
+# auto model    - 0.4049
+# yee-haa
+
+
+#------------------------------------------
+# Model testing
+#------------------------------------------
+
+# split the data in train and test
+# 83 obvs
+# Set last 18 months as the test dataset
+
+items_train <- stats::window(x = ts_monthly, end = c(2018, 8) )
+items_train
+str(items_train)
+
+items_test <- stats::window(x = ts_monthly, start = c(2018, 9) )
+items_test
+
+man_trained_arima_items <-arima(items_train, 
+                    c(0, 1, 2),
+                    seasonal = list(order = c(1, 0, 0),
+                                    period = 12)
+)
+man_trained_arima_items
+
+
+auto_arima_model <- auto.arima(items_train)
 auto_arima_model
 
-forecast_model <- forecast(auto_arima_model, 
-                           level = c(95), # 36 momnths (though could be 3 * 16), 
-                           h = 12) # h is confidence interval
-forecast_model
-plot(forecast(forecast_model, 3),
-     ylim=c(0,200000))
+predict_auto_ARIMA <- forecast(auto_arima_model, 18)
+predict_auto_ARIMA
+
+man_predict <- forecast(man_trained_arima_items, 18)
+man_predict
 
 
 
-ts_monthly_items = ts(data_in$Total_Items,
-                start = c(2013, 4),
-                frequency = 12 # monthly data
-)
-ts_monthly_items
+actuals_predictions <- data.frame(cbind(actuals = items_test, predicted = predict_auto_ARIMA))
+head(actuals_predictions)
 
-ts_monthly_patients = ts(data_in$Total_Patients,
-                start = c(2013, 4),
-                frequency = 12 # monthly data
-)
-ts_monthly_patients
+correlation_accuracy <- cor(actuals_predictions)
+correlation_accuracy
 
 
-ts_monthly_ratio = ts(data_in$item_patient_ratio , 
-                start = c(2013, 4),
-                frequency = 12 # monthly data
-)
-ts_monthly_ratio
+actuals_predictions_man <- data.frame(cbind(actuals = items_test, predicted = precict_manual_ARIMA))
+head(actuals_predictions_man)
+str(actuals_predictions_man)
+
+correlation_accuracy_man <- cor(actuals_predictions_man)
+correlation_accuracy_man
 
 
+actuals_predictions_man <- data.frame(cbind(actuals = items_test, predicted = precict_manual_ARIMA))
 
-# -----------------------------------------------------
-
-auto_arima_model <- auto.arima(ts_monthly_ratio)
-auto_arima_model
-
-forecast_model <- forecast(auto_arima_model, 
-                           level = c(95), # 36 momnths (though could be 3 * 16), 
-                           h = 8) # h is confidence interval
-forecast_model
-plot(forecast(forecast_model, 4))
+predict_auto_ARIMA$g <- "Auto"
+precict_manual_ARIMA$g <- "Man"
+items_test$g <- "Actual"
 
 
-auto_arima_model <- auto.arima(ts_monthly_items)
-auto_arima_model
+# plot all three
 
-forecast_model <- forecast(auto_arima_model, 
-                           level = c(95), # 36 momnths (though could be 3 * 16), 
-                           h = 8) # h is confidence interval
-forecast_model
-plot(forecast(forecast_model, 4))
-
-# auto_arima_model <- auto.arima(ts_monthly_patients)
-# auto_arima_model
-# 
-# forecast_model <- forecast(auto_arima_model, 
-#                            level = c(95), # 36 momnths (though could be 3 * 16), 
-#                            h = 8) # h is confidence interval
-# forecast_model
-# plot(forecast(forecast_model, 4))
+items_test <- stats::window(x = ts_monthly, start = c(2018, 9) )
+man2 <- stats::window(x = man_predict, start = c(2018, 9) )
 
 
-# plot(forecast(forecast_model, 3),
-#      ylim=c(0,2) )
+par(opar)
+plot(items_test)
+plot(man_predict)
 
+par(new = TRUE)
+plot(man_predict, type = "l", axes = FALSE, bty = "n", xlab = "", ylab = "", col = "red",
+     main = "Data plot with log of data overlay")
+
+par(opar)
+
+
+#------------------------------------------
+# EOF
+#------------------------------------------
 
